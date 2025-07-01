a3d6fd04482eb1d6071899be7eb3eb4dcc3d4e44
Pablo Soares Herzberg <pabloherzberg@gmail.com>
2025-07-01 13:47:04 +0000

    ao tentar executar no meu computador o projeto isso acontece
    $ npm run

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    instrumentationHook: true,
+  },
+};
+
+export default nextConfig;

cec58a1004a25dc76cc339517724bd9177be07f2
Pablo Soares Herzberg <pabloherzberg@gmail.com>
2025-07-16 02:00:27 +0000

    feat: delete useTenantForm

--- src/hooks/useTenantForm.ts
+++
@@ -1,3 +0,0 @@
-// This file has been removed as its logic has been simplified and
-// integrated into useAdminManagement.ts and the TenantsTab.tsx component.
-// This centralization improves code clarity and reduces complexity.

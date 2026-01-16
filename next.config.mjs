eb53a2a62acf740d93d7379f19dda10c0a5acc92
Pablo Herzberg <pabloherzberg@gmail.com>
2026-01-16 15:30:48 -0300

    fix: vercel ignore cache

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    compiler: {
-        // A opção `removeConsole` quando definida como `false` impede a remoção dos logs.
-        removeConsole: false,
-    },
-    // Desabilitar cache durante build na Vercel
-    generateBuildId: async () => {
-        return `build-${Date.now()}`
-    },
+  compiler: {
+    // A opção `removeConsole` quando definida como `false` impede a remoção dos logs.
+    removeConsole: false,
+  },
+  // Desabilitar cache durante build na Vercel
+  generateBuildId: async () => {
+    return `build-${Date.now()}`;
+  },
 };
 
 export default nextConfig;

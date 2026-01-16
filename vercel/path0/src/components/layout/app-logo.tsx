d2657f36980882fbbcd066735681b2cda668e481
Pablo Herzberg <pabloherzberg@gmail.com>
2026-01-16 18:33:15 -0300

    fix: remove vercel folder

--- vercel/path0/src/components/layout/app-logo.tsx
+++
@@ -1,7 +0,0 @@
-
-import { ShieldCheck } from 'lucide-react';
-import type { LucideProps } from 'lucide-react';
-
-export function AppLogo(props: LucideProps) {
-    return <ShieldCheck className="h-8 w-8 text-orange-500" {...props} />;
-}
